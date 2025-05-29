92f1e196b26675000f9e081dc1fa638d6f6247ab
Yurkes2020 <yurkes2020@gmail.com>
2025-05-29 22:02:05 +0300

    done fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import React from "react";
-import Link from "next/link";
 import {Menu} from "@/app/components/Menu";
 
 const geistSans = Geist({
